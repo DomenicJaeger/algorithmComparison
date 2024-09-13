43344228e18d8790b18b2e22e053b0dd98a5296d
Domenic <domeinc_jaeger@teilnehmer.cdemy.de>
2024-09-13 12:14:56 +0200

    sorter comments

--- lib/models/sorter.dart
+++ lib/models/sorter.dart
@@ -2,19 +2,23 @@ import 'package:algorithm_comparison/functions/sorting/run_bubble_sort.dart';
 import 'package:algorithm_comparison/functions/sorting/run_merge_sort.dart';
 import 'package:algorithm_comparison/functions/sorting/run_quick_sort.dart';
 
+// class to sort the random numbers
 class Sorter {
   List<int> random;
 
   Sorter(this.random);
 
+// run the sorting algorithms
   List<int> runBubbleSort() {
     return bubbleSort(random);
   }
 
+// run the sorting algorithms
   List<int> runMergeSort() {
     return mergeSort(random);
   }
 
+// run the sorting algorithms
   List<int> runQuickSort() {
     return quickSort(random);
   }
